325cd252f7d6646a1c5860278bb114f13defaaea
facumasaUY <facundomasa@gmail.com>
2024-10-30 00:29:48 +0000

    Final project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,24 +9,24 @@ const Home = () => {
 
 	return (
 		<div className="container mt-5">
-			<h1 className="text-center mt-5">Todos</h1>
+			<h1 className="title text-center mt-5">Todos</h1>
 			<div className="mx-auto col-6">
-				<input placeholder="What needs to be done?" type="text" className="form-control" 
+				<input placeholder="What needs to be done?" type="text" className="input form-control rounded-0" 
 				value={nuevaTarea}onChange={(evento)=> {
 					setNuevaTarea(evento.target.value)
 				}}
 				onKeyUp={(evento)=>{
-					if (evento.key == "Enter"){
+					if (evento.key == "Enter" && nuevaTarea.trim() !== ""){
 						setListaDeTareas([...listaDeTareas, nuevaTarea])
 						setNuevaTarea("")
 					}
 					console.log(evento.key)
 				}}
 				/>
-			    <ul>
+			    <ul className="m-0 p-0">
 					{listaDeTareas.map((item,index) => {
 						return(
-                           <li key={index}>  {item}<i onClick={()=>{
+                           <li className="lista border-bottom border-start border-end d-flex justify-content-between" key={index}>  <span className="xButton">{item}</span><i className="" onClick={()=>{
 							const aux = listaDeTareas.filter((task, ind) => {
 								return (ind != index)
 							})
@@ -36,9 +36,13 @@ const Home = () => {
 						})} 
 					
 				</ul>
-				<span>
-					{listaDeTareas.length} Items left
+				<div className="bottomDiv border-bottom border-start border-end text-muted">
+				<span className="spanBottom">
+					{listaDeTareas.length > 0 ? `${listaDeTareas.length} Items left` : "No more tasks. Add a new task"}
 				</span>
+				</div>
+				<div className="mx-2 py-1 border-bottom border-start border-end"></div>
+				<div className="mx-3 py-1 border-bottom border-start border-end"></div>
 		    </div>
 
 		</div>
